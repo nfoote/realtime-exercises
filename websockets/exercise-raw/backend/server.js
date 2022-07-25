e92276bde4cacf8421259a205cc8868a97823b10
Nick <nfoote@utas.edu.au>
2022-07-25 21:13:28 +1000

    finish websocket raw section

--- websockets/exercise-raw/backend/server.js
+++ websockets/exercise-raw/backend/server.js
@@ -24,11 +24,49 @@ const server = http.createServer((request, response) => {
   });
 });
 
-/*
- *
- * your code goes here
- *
- */
+server.on("upgrade", function (req, socket) {
+  if (req.headers["upgrade"] !== "websocket") {
+    // we only care about websockets
+    socket.end("HTTP/1.1 400 Bad Request");
+    return;
+  }
+
+  const acceptKey = req.headers["sec-websocket-key"];
+  const acceptValue = generateAcceptValue(acceptKey);
+  const headers = [
+    "HTTP/1.1 101 Web Socket Protocol Handshake",
+    "Upgrade: WebSocket",
+    "Connection: Upgrade",
+    `Sec-WebSocket-Accept: ${acceptValue}`,
+    "Sec-WebSocket-Protocol: json",
+    "\r\n",
+  ];
+  
+  socket.write(headers.join("\r\n"));
+  socket.write(objToResponse({ msg: getMsgs() }));
+  connections.push(socket);
+
+  socket.on("data", (buffer) => {
+    const message = parseMessage(buffer);
+    if (message) {
+      msg.push({
+        user: message.user,
+        text: message.text,
+        time: Date.now(),
+      });
+      connections.forEach((s) => s.write(objToResponse({ msg: getMsgs() })));
+    }
+    else if (message === null) {
+      // remove from my active connections
+      socket.end();
+    }
+  });
+
+  socket.on("end", () => {
+    console.log("end")
+    connections = connections.filter((s) => s !== socket);
+  });
+});
 
 const port = process.env.PORT || 8080;
 server.listen(port, () =>
